6766ce3045928d9af15552215a342c2ac1330c8e
Endyzi <private_nilhamn@hotmail.com>
2025-02-02 15:04:10 +0100

    unit test for hb mutation

--- api/tests/Feature/SendHeartbeatTest.php
+++ api/tests/Feature/SendHeartbeatTest.php
@@ -2,19 +2,49 @@
 
 namespace Tests\Feature;
 
+use App\Models\Heartbeat;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SendHeartbeatTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test for the sendheartbeatmutation
      */
-    public function test_example(): void
+    public function test_send_heartbeat_creates_or_updates_record()
     {
-        $response = $this->get('/');
+        $response = $this->graphql('
+        mutation{
+        sendHeartbeat(input: {
+         applicationKey: "app-1",
+         heartbeatKey: "sync-job",
+         unhealthyAfterMinutes: 5
+        }) {
+         hearbeat {
+          applicationKey
+          heartbeatKey
+          unhealthyAfterMinutes
+          lastCheckIn
+        }
+       }
+      }
+    ');
 
-        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'sendheartbeat' => [
+                    'heartbeat' => [
+                        'applicationKey' => 'app-1',
+                        'heartbeatKey' => 'sync-job',
+                        'unhealthyAfterMinutes' => 5,
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertDatabaseHas('heartbeats', [
+            'application_Key' => 'app-1',
+            'heartbeat_key' => 'sync-job',
+        ]);
     }
 }
