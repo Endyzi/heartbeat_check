6505a94edc3d55568ced2ef8e9ce39df32cac0b6
Endyzi <private_nilhamn@hotmail.com>
2025-02-03 11:31:13 +0100

    added user model and table thats needed for sanctum

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 }
