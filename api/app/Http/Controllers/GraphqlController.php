6836c6e4eaa3746fc6beb8aa37d203b71b2a411d
Endyzi <private_nilhamn@hotmail.com>
2025-02-04 15:18:25 +0100

    graphqlcontroller, needed to use butler-api

--- api/app/Http/Controllers/GraphqlController.php
+++ api/app/Http/Controllers/GraphqlController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
 use Butler\Graphql\Concerns\HandlesGraphqlRequests;
 use GraphQL\Type\Schema;
 use GraphQL\Language\AST\DocumentNode;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
 
+/**
+ * GraphController is needed or recommended in order to use the butler api
+ */
 class GraphqlController extends Controller
 {
     use HandlesGraphqlRequests;
-
+    //this hook is used in api/vendor/glesys/butler-service/src/routes.php 
+    //haven't figured out the unauthenticated problem, even if bearer token is created. 
     public function beforeExecutionHook(Schema $schema, DocumentNode $query, string $operationName = null, $variables = null): void
     {
         // check authentication to sanctum
